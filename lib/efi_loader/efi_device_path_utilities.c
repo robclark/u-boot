07482c0a457b50f5634ce5d300bde682ee4c642e
Rob Clark <robdclark@gmail.com>
2017-09-03 10:35:25 -0400

    efi_loader: start fleshing out efi_device_path_utilities
    Not complete, but enough for Shell.efi and SCT.efi.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- lib/efi_loader/efi_device_path_utilities.c
+++ lib/efi_loader/efi_device_path_utilities.c
@@ -12,36 +12,40 @@
 const efi_guid_t efi_guid_device_path_utilities_protocol =
 		EFI_DEVICE_PATH_UTILITIES_PROTOCOL_GUID;
 
-static UINTN get_device_path_size(const struct efi_device_path *device_path)
+static UINTN EFIAPI get_device_path_size(const struct efi_device_path *device_path)
 {
+	UINTN sz = 0;
 	EFI_ENTRY("%p", device_path);
-	return EFI_EXIT(0);
+	/* size includes the END node: */
+	if (device_path)
+		sz = efi_dp_size(device_path) + sizeof(struct efi_device_path);
+	return EFI_EXIT(sz);
 }
 
-static struct efi_device_path *duplicate_device_path(
+static struct efi_device_path * EFIAPI duplicate_device_path(
 	const struct efi_device_path *device_path)
 {
 	EFI_ENTRY("%p", device_path);
-	return EFI_EXIT(NULL);
+	return EFI_EXIT(efi_dp_dup(device_path));
 }
 
-static struct efi_device_path *append_device_path(
+static struct efi_device_path * EFIAPI append_device_path(
 	const struct efi_device_path *src1,
 	const struct efi_device_path *src2)
 {
 	EFI_ENTRY("%p, %p", src1, src2);
-	return EFI_EXIT(NULL);
+	return EFI_EXIT(efi_dp_append(src1, src2));
 }
 
-static struct efi_device_path *append_device_node(
+static struct efi_device_path * EFIAPI append_device_node(
 	const struct efi_device_path *device_path,
 	const struct efi_device_path *device_node)
 {
 	EFI_ENTRY("%p, %p", device_path, device_node);
-	return EFI_EXIT(NULL);
+	return EFI_EXIT(efi_dp_append_node(device_path, device_node));
 }
 
-static struct efi_device_path *append_device_path_instance(
+static struct efi_device_path * EFIAPI append_device_path_instance(
 	const struct efi_device_path *device_path,
 	const struct efi_device_path *device_path_instance)
 {
@@ -49,7 +53,7 @@ static struct efi_device_path *append_device_path_instance(
 	return EFI_EXIT(NULL);
 }
 
-static struct efi_device_path *get_next_device_path_instance(
+static struct efi_device_path * EFIAPI get_next_device_path_instance(
 	struct efi_device_path **device_path_instance,
 	UINTN *device_path_instance_size)
 {
@@ -57,14 +61,14 @@ static struct efi_device_path *get_next_device_path_instance(
 	return EFI_EXIT(NULL);
 }
 
-static struct efi_device_path *create_device_node(
+static struct efi_device_path * EFIAPI create_device_node(
 	uint8_t node_type, uint8_t node_sub_type, uint16_t node_length)
 {
 	EFI_ENTRY("%u, %u, %u", node_type, node_sub_type, node_length);
 	return EFI_EXIT(NULL);
 }
 
-static bool is_device_path_multi_instance(
+static bool EFIAPI is_device_path_multi_instance(
 	const struct efi_device_path *device_path)
 {
 	EFI_ENTRY("%p", device_path);
